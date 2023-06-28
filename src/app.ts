35275bc0b2589ef9757d7a0d47632e5ad2f852c2
doyindev1 <ade.doyin27@gmail.com>
2023-06-28 22:20:04 +0100

    autobinding in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 // Code goes here!
 
+function autobind(
+    _: any,
+     _2: string,
+      descriptor: PropertyDescriptor ) {
+
+const originalMethod = descriptor.value;
+const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+        const boundFn = originalMethod;
+        return boundFn;
+    }
+};
+return adjDescriptor
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -21,9 +37,40 @@ class ProjectInput {
          this.configure();
          this.attach();
     }
+
+        private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDesription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;    
+
+    if (enteredTitle.trim().length === 0 || 
+        enteredPeople.trim().length === 0 ||
+        enteredPeople.trim().length === 0) {
+    alert('Invalid input, please try again!')
+      return
+   } else {
+    return [enteredTitle, enteredDesription, +enteredPeople];
+}
+
+        }
+        private clearInputs() {
+            this.titleInputElement.value = '';
+            this.descriptionInputElement.value = '';
+            this.peopleInputElement.value = '';
+            
+        }
+
+
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        // console.log(this.titleInputElement.value);
+        if (Array.isArray(userInput)){
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
     private configure(){
         this.element.addEventListener('submit', this.submitHandler.bind(this));
